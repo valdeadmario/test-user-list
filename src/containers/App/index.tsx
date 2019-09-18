ff65ec534fcb4d4ac34ce6f9b1ea054ff21108fa
Valdeadmario <vladimir.vstup@gmail.com>
2019-09-18 21:28:02 +0300

    Change style

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Provider } from "react-redux";
 
 import UserList from "../UserList";
+import Header from "../../components/Header";
 
 import store from "../../store";
+
+import "./style.css";
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
+      <Header />
       <UserList />
     </Provider>
   );
