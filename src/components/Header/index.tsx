53ebbd3cabddd2afe6ee6c16db6e77d1f28f199a
Valdeadmario <vladimir.vstup@gmail.com>
2019-09-18 20:39:32 +0300

    Create and style header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <a className="logo" href="/">
+        UserList
+      </a>
+    </header>
+  );
+};
+
+export default Header;
