78f70ae2ce98107fbfb368aba415f77f0f192279
Valdeadmario <vladimir.vstup@gmail.com>
2019-09-18 20:39:44 +0300

    Add Spinner

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.css";
+
+const Spinner = () => (
+  <div className="lds-css ng-scope">
+    <div className="lds-rolling">
+      <div />
+    </div>
+  </div>
+);
+
+export default Spinner;
